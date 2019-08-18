430271972ba79497a2ab73758b408825aab2b1c4
yangyunxin <yangyunxin@fangdd.com>
2019-08-18 23:50:35 +0800

    ding

--- src/container/Order/columns/operator.js
+++ src/container/Order/columns/operator.js
@@ -1,33 +1,42 @@
+import { ORDER_STATUS  } from '../../../utils/constant';
+import { formatDateSecond  } from '../../../utils/utils';
+
+const paymentStatus = ['支付成功', '支付失败'];
+
 const columns = [
   {
     title: '快递公司',
-    dataIndex: 'mainPicture',
-    key: 'mainPicture',
+    dataIndex: 'expressCompanyName',
+    key: 'expressCompanyName',
     align: 'center',
   },
   {
     title: '操作时间',
-    dataIndex: 'name',
-    key: 'name',
+    dataIndex: 'shippingTime',
+    key: 'shippingTime',
     align: 'center',
+    render: text => text && formatDateSecond(text)
   },
   {
     title: '订单状态',
-    dataIndex: 'productPrice',
-    key: 'productPrice',
+    dataIndex: 'status',
+    key: 'status',
     align: 'center',
+    render: text => ORDER_STATUS[text] || '未支付'
   },
   {
     title: '支付状态',
-    dataIndex: 'unit',
-    key: 'unit',
+    dataIndex: 'paymentStatus',
+    key: 'paymentStatus',
     align: 'center',
+    render: text => paymentStatus[text],
   },
   {
     title: '发货状态',
-    dataIndex: 'colour',
-    key: 'colour',
+    dataIndex: 'expressTrackingNo',
+    key: 'expressTrackingNo',
     align: 'center',
+    render: text => text ? '已发货' : '未发货',
   },
   {
     title: '备注',
